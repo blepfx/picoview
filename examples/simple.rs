f01bba8299f260b9f1ac5b91da9c9dba5d0dc4e8
Quant1um <spitfirexv22@gmail.com>
2025-08-18 15:25:03 +0400

    clean up simple.rs example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,5 @@
 use picoview::{Event, EventResponse, MouseCursor, Point, Size, WindowBuilder};
-use std::{
-    thread,
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
 fn main() {
     WindowBuilder::new({
@@ -10,54 +7,63 @@ fn main() {
         let mut last = Instant::now();
 
         move |event, mut window| {
-            if matches!(event, Event::WindowOpen) {
-                window.set_cursor_icon(MouseCursor::Move);
-                println!("clipboard contents: {:?}", window.get_clipboard_text());
-                window.set_clipboard_text("delta");
-            } else if matches!(event, Event::WindowFrame { .. }) {
-                println!("frame: {:?}", start.elapsed());
-
-                let passed = |d| {
-                    start.elapsed() >= Duration::from_millis(d)
-                        && (last - start) < Duration::from_millis(d)
-                };
-
-                if passed(5000) {
-                    println!("Resize window");
-                    window.set_title("picoview - example");
-                    window.set_size(Size {
-                        width: 300,
-                        height: 300,
-                    });
-
-                    WindowBuilder::new(|event, _| {
-                        if !matches!(event, Event::WindowFrame { .. }) {
-                            println!("child {:?}", event);
-                        }
-
-                        EventResponse::Captured
-                    })
-                    .open_parented(&window)
-                    .unwrap();
+            match event {
+                Event::WindowOpen => {
+                    window.set_cursor_icon(MouseCursor::Move);
+                    println!("clipboard contents: {:?}", window.get_clipboard_text());
+                    window.set_clipboard_text("test");
                 }
 
-                if passed(15000) {
-                    println!("Closing window");
+                Event::WindowFrame { .. } => {
+                    let passed = |d| {
+                        start.elapsed() >= Duration::from_millis(d)
+                            && (last - start) < Duration::from_millis(d)
+                    };
+
+                    if passed(5000) {
+                        println!("Resize window");
+                        window.set_title("picoview - example");
+                        window.set_size(Size {
+                            width: 300,
+                            height: 300,
+                        });
+
+                        WindowBuilder::new(|event, _| {
+                            if !matches!(event, Event::WindowFrame { .. }) {
+                                println!("child {:?}", event);
+                            }
+
+                            EventResponse::Captured
+                        })
+                        .open_parented(&window)
+                        .unwrap();
+                    }
+
+                    if passed(15000) {
+                        println!("Closing window");
+                        window.close();
+                    }
+
+                    last = Instant::now();
+                }
+
+                Event::MouseMove { cursor } => {
+                    if let Some(cursor) = cursor
+                        && cursor.x < -10.0
+                    {
+                        window.set_cursor_position(Point { x: 100.0, y: 100.0 });
+                    }
+
+                    println!("{:?}", event);
+                }
+
+                Event::WindowClose => {
                     window.close();
                 }
 
-                last = Instant::now();
-            } else if let Event::MouseMove {
-                cursor: Some(cursor),
-            } = event
-            {
-                if cursor.x < -10.0 {
-                    window.set_cursor_position(Point { x: 100.0, y: 100.0 });
+                _ => {
+                    println!("{:?}", event);
                 }
-
-                println!("{:?}", event);
-            } else {
-                println!("{:?}", event);
             }
 
             picoview::EventResponse::Rejected
@@ -69,7 +75,4 @@ fn main() {
     .unwrap();
 
     println!("Exiting loop");
-
-    thread::sleep(Duration::from_millis(5000));
-    println!("Closing app");
 }
