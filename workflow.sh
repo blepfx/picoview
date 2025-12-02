6d49db2c5546b008dfcb13a90ecefc6d4ad90fb9
Quant1um <blep@amee.ee>
2025-12-02 18:00:14 +0400

    still doing ci stuff (3)

--- workflow.sh
+++ workflow.sh
@@ -1,9 +1,9 @@
 export RUSTFLAGS="-Dwarnings -Zsanitizer=address"
 export RUSTDOCFLAGS="-Dwarnings"
 
-cargo build --workspace --all-targets
-cargo build --workspace --all-targets --all-features
-cargo test --workspace --all-targets --all-features
+cargo build -Zbuild-std --workspace --all-targets
+cargo build -Zbuild-std --workspace --all-targets --all-features
+cargo test -Zbuild-std --workspace --all-targets --all-features
 cargo doc --examples --all-features --no-deps
 cargo clippy --workspace --all-targets --all-features
 cargo fmt --all -- --check
