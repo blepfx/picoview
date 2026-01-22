3ccc2aaa86320a73603e14065e0ed906307c6ec3
Quant1um <blep@amee.ee>
2026-01-22 18:11:39 +0400

    opengl example: draw test grid

--- examples/opengl.rs
+++ examples/opengl.rs
@@ -1,5 +1,5 @@
-use picoview::{Event, GlConfig, Point, WindowBuilder};
-use std::{mem::transmute, time::Instant};
+use picoview::{Event, GlConfig, GlVersion, Point, Size, WindowBuilder};
+use std::mem::transmute;
 
 fn main() {
     WindowBuilder::new(|window| {
@@ -8,24 +8,56 @@ fn main() {
             unsafe { transmute(gl.get_proc_address(c"glClearColor")) };
         let clear: unsafe extern "system" fn(i32) =
             unsafe { transmute(gl.get_proc_address(c"glClear")) };
+        let begin: unsafe extern "system" fn(i32) =
+            unsafe { transmute(gl.get_proc_address(c"glBegin")) };
+        let end: unsafe extern "system" fn() = unsafe { transmute(gl.get_proc_address(c"glEnd")) };
+        let vertex: unsafe extern "system" fn(f32, f32, f32) =
+            unsafe { transmute(gl.get_proc_address(c"glVertex3f")) };
+        let viewport: unsafe extern "system" fn(i32, i32, i32, i32) =
+            unsafe { transmute(gl.get_proc_address(c"glViewport")) };
+        let color: unsafe extern "system" fn(f32, f32, f32, f32) =
+            unsafe { transmute(gl.get_proc_address(c"glColor4f")) };
 
-        let mut last_frame = Instant::now();
-        let mut time = 0.0;
+        let mut size = Size {
+            width: 200,
+            height: 200,
+        };
 
         Box::new(move |event| match event {
             Event::WindowFrame => unsafe {
-                time += last_frame.elapsed().as_secs_f32();
-                last_frame = Instant::now();
+                if !gl.make_current(true) {
+                    return;
+                }
 
-                gl.make_current(true);
-
-                (clear_color)(
-                    (time + 0.0).sin().abs(),
-                    (time + 2.0).sin().abs(),
-                    (time + 4.0).sin().abs(),
-                    (time + 6.0).sin().abs(),
-                );
+                (clear_color)(0.25, 0.25, 0.25, 0.5);
                 (clear)(0x00004000);
+                (viewport)(0, 0, size.width as i32, size.height as i32);
+
+                let draw_line = |x1: f32, y1: f32, x2: f32, y2: f32| {
+                    let x1 = (x1 / size.width as f32) * 2.0 - 1.0;
+                    let y1 = 1.0 - (y1 / size.height as f32) * 2.0;
+                    let x2 = (x2 / size.width as f32) * 2.0 - 1.0;
+                    let y2 = 1.0 - (y2 / size.height as f32) * 2.0;
+
+                    (vertex)(x1, y1, 0.0);
+                    (vertex)(x2, y2, 0.0);
+                };
+
+                (begin)(0x0001); // GL_LINES
+
+                (color)(0.5, 0.5, 0.5, 0.5);
+                for i in (0..1000).step_by(25) {
+                    draw_line(i as f32, 0.0, i as f32, size.height as f32);
+                    draw_line(0.0, i as f32, size.width as f32, i as f32);
+                }
+
+                (color)(1.0, 1.0, 1.0, 1.0);
+                for i in (0..1000).step_by(100) {
+                    draw_line(i as f32, 0.0, i as f32, size.height as f32);
+                    draw_line(0.0, i as f32, size.width as f32, i as f32);
+                }
+
+                (end)();
 
                 gl.swap_buffers();
                 gl.make_current(false);
@@ -42,11 +74,18 @@ fn main() {
                 window.close();
             }
 
+            Event::WindowResize { size: new_size } => {
+                size = new_size;
+            }
+
             event => println!("{:?}", event),
         })
     })
     .with_title("OpenGL Example")
-    .with_opengl(GlConfig::default())
+    .with_opengl(GlConfig {
+        version: GlVersion::Compat(2, 1),
+        ..Default::default()
+    })
     .with_size((200, 200))
     .with_resizable((0, 0), (1000, 1000))
     .with_transparency(true)
